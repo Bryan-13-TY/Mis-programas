362113b4eb678574567170160df99b5832824322
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2025-10-29 20:43:35 -0600

    Práctica 3 de redes: Archivos de prueba 3

--- Programas en Python/Discord en python/Archivos prueba/Grabar audio/grabaraudio.py
+++ Programas en Python/Discord en python/Archivos prueba/Grabar audio/grabaraudio.py
@@ -6,7 +6,7 @@ from pathlib import Path
 # Ruta
 carpetaScript = Path(__file__).parent
 fecha = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-rutaAudio = carpetaScript/".."/"audio"/f"grabacion_{fecha}.wav"
+rutaAudio = carpetaScript/"audio"/f"grabacion_{fecha}.wav"
 
 # Crear carpeta si no existe
 rutaAudio.parent.mkdir(parents=True, exist_ok=True)
