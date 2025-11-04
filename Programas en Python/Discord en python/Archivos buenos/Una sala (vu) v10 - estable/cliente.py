b3b9e62b748d214fc7a1f6b675da75eebe20ed5c
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2025-11-04 07:10:38 -0600

    Práctica 3 de redes: Versión más estable del servidor.py y cliente.py 👽
    Estas versiones del servidor.py y cliente.py, únicamente trabajan en una sola sala, los envíos a todos los usuarios en la sala es correcto, se indica cuando un usuario sale y entra de la sala, los stickers en este caso serán los emojis, y se usa el comando /salir para abandonar la sala. Los mensajes privados en una sala funcionan bien, se usa el formato @usuario mensaje para ello. Para grabar un audio se usa el comando /audio, el audio se graba y se reproduce correctamente. Ya se implementaron los stickers privados, falta los audios privados.
    
    Por el momento las distintas salas no han sido implementadas y el modo con interfaz gráfica se descarta.

---
+++ Programas en Python/Discord en python/Archivos buenos/Una sala (vu) v10 - estable/cliente.py
@@ -0,0 +1,326 @@
+import socket, threading, json, sys, sounddevice as sd, wavio
+from datetime import datetime
+from pathlib import Path
+
+SERVER_IP, SERVER_PORT = "127.0.0.1", 5007  # IP y puerto del servidor
+
+# Colores ANSI
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+MAGENTA = "\033[95m"
+ORANGE = "\033[33m"
+RESET = "\033[0m"
+
+class Cliente:
+    def __init__(self, usuario: str, sala: str) -> None:
+        """
+        Método principal de la clase.
+
+        Parameters
+        ----------
+        usuario : str
+            Nombre de usuario del cliente.
+        sala : srt
+            Nombre de la sala en la que se encuentra el cliente.
+        """
+        self.usuario = usuario
+        self.sala = sala
+        self.activo = True
+
+        # Se crea el socket UDP para enviar y recibir mensajes
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Se crea un socket UDP
+        self.sock.bind(('', 0))  # Puerto aleatorio para recibir mensajes
+
+        # Avisar al servidor que un usuario entro a la sala
+        inicio = {"tipo": "inicio",
+                  "user": self.usuario,
+                  "sala": self.sala}
+        
+        self.sock.sendto(json.dumps(inicio).encode(), (SERVER_IP, SERVER_PORT))
+
+    def grabar_audio(self) -> None:
+        """Método para grabar un audio, reproducirlo y posteriormente guardarlo."""
+        # Se construye la ruta donde se guardara la grabación
+        carpeta_script = Path(__file__).parent
+        fecha = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        ruta_grabacion = carpeta_script/f"{self.sala}"/f"grabacion_{self.usuario}_{fecha}.wav"
+        ruta_grabacion.parent.mkdir(parents=True, exist_ok=True) # Se crea la carpeta si no existe
+
+        # Parámetros
+        duracion, frecuencia = (5, 44100) # (segundos, Hz 'calidad estándar de audio')
+
+        print("[Sistema] Grabando audio...")
+
+        audio = sd.rec(int(duracion * frecuencia), samplerate=frecuencia, channels=2)
+
+        sd.wait() # Esperar a que termine la grabación
+        print("[Sistema] Grabación completa")
+        wavio.write(str(ruta_grabacion), audio, frecuencia, sampwidth=2) # Guardar la grabación
+
+        #print(f"Audio guardado como '{ruta_grabacion}")
+        print("[Sistema] Reproduciendo grabación...")
+
+        sd.play(audio, frecuencia) # Reproducir la grabación
+        sd.wait() # Esperar a que termine la reproducción
+
+        print("[Sistema] Reproducción terminada")
+
+    def recibir(self) -> None:
+        """Método para recibir mensajes enviados desde el servidor."""
+        while (self.activo): # Mientras el usuario siga en una sala
+            try:
+                data, _ = self.sock.recvfrom(4096)
+                mensaje = json.loads(data.decode())
+                sala_mensaje = mensaje.get("sala", self.sala)
+                tipo = mensaje["tipo"]
+
+                if (sala_mensaje != self.sala):
+                    # Si el mensaje no se envía a la sala actual se ignoras
+                    continue
+
+                if (tipo == "msj"): # Si es un mensaje
+                    if (mensaje.get("privado")): # Si es un mensaje privado
+                        print(f"{YELLOW}[Privado de {mensaje['from']}]{RESET}: {mensaje['content']}")
+                    else: # Si es un mensaje público
+                        print(f"{BLUE}[{mensaje['user']}]{RESET}: {mensaje['content']}")
+                
+                if (tipo == "aviso"): # Si es un avio
+                    print(mensaje["content"])
+                
+                if (tipo == "usuarios"): # Si es la lista de usuarios conectados
+                    print(f"\nUsuarios en sala {MAGENTA}'{self.sala}'{RESET}: {MAGENTA}{', '.join(mensaje['lista'])}{RESET}\n")
+            except:
+                break
+
+    def sticker(self, nombre_sticker: str) -> str:
+        """
+        Método para seleccionar el sticker.
+        
+        Parameters
+        ----------
+        nombre_sticker : str
+            Nombre del sticker seleccioando.
+        """
+        match (nombre_sticker):
+            case 'shrek':
+                sticker_enviar = """
+⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀ ⣀⣀⣤⣤⣤⣀⡀
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠸⣼⡿
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇
+   ⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+"""
+            case 'heisenberg':
+                sticker_enviar = """
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠜⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡏⠁⠀⠀⠀⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⣦⣤⡄⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡧⠇⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣮⣭⣿⡻⣽⣒⠀⣤⣜⣭⠐⢐⣒⠢⢰⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⡟⣾⣿⠂⢈⢿⣷⣞⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣷⣶⣾⡿⠿⣿⠗⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠻⠋⠉⠑⠀⠀⢘⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢹⣿⣿⡇⢀⣶⣶⠴⠶⠀⠀⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⢸⣿⣿⠀⠀⠣⠀⠀⠀⠀⠀⡟⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠹⣿⣧⣀⠀⠀⠀⠀⡀⣴⠁⢘⡙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⠗⠂⠄⠀⣴⡟⠀⠀⡃⠀⠉⠉⠟⡿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠾⠛⠂⢹⠀⠀⠀⢡⠀⠀⠀⠀⠀⠙⠛⠿⢿⣿⣿⣿⣿⣿⣿
+"""
+            case 'dog':
+                sticker_enviar = """
+ / \\__
+(    @\\___
+ /         O
+/   (_____/
+/_____/   U
+"""
+            case 'cat':
+                sticker_enviar = """
+ /\\_/\\
+( o.o )
+ > ^ <
+"""
+            case 'heart':
+                sticker_enviar = """
+  **     **
+ *****  *****
+**************
+ ************
+  **********
+    ******
+      **
+"""
+            case 'rocket':
+                sticker_enviar = """
+    ^
+   /^\\
+  /___\\
+  |= |=
+  |.-.|
+  |'-'|
+  |   |
+ /|_|_|\\
+   / \\
+  /___\\
+"""
+            case 'rabbit':
+                sticker_enviar = """
+ (\\_/)
+ ( •_•)
+ / >🍪
+"""
+            case 'owl':
+                sticker_enviar = """
+ ,_,  
+(O,O) 
+(   ) 
+ " "
+"""
+            case 'dinosaur':
+                sticker_enviar = """
+           __
+          / _)
+   .-^^^-/ /
+__/       /
+<__.|_|-|_|
+"""
+            case _:
+                return ""
+        
+        return sticker_enviar
+            
+    def enviar(self) -> None:
+        """Método para enviar mensajes al servidor."""
+        while (self.activo): # Mientras el usuario siga en una sala
+            try:
+                texto_entrada = input("").strip()
+                
+                if (texto_entrada.lower() == "/salir"): # Si quiere salir de la sala
+                    salir = {"tipo": "salir",
+                             "user": self.usuario,
+                             "sala": self.sala}
+                    
+                    self.sock.sendto(json.dumps(salir).encode(), (SERVER_IP, SERVER_PORT))
+                    self.activo = False
+                    
+                    print("[Sistema] Has salido de la sala")
+                    break
+                elif (texto_entrada.lower() == "/audio"): # Si quiere grabar un audio
+                    audio = {"tipo": "audio",
+                             "user": self.usuario,
+                             "sala": self.sala}
+                    
+                    self.grabar_audio()
+                    self.sock.sendto(json.dumps(audio).encode(), (SERVER_IP, SERVER_PORT))
+                elif ((texto_entrada.lower()).split(" ", 1)[0] == "/sticker"): # Si es un sticker público
+                    partes = texto_entrada.split(" ", 1)
+
+                    if (len(partes) < 2): # Si el formato no es el correcto
+                        print("[Sistema] Formato: /sticker nombre_sticker")
+                        continue
+
+                    _, nombre_sticker = partes
+
+                    sticker = self.sticker(nombre_sticker)
+
+                    if (not sticker): # Si el sticker no existe
+                        print("[Sistema] El sticker no existe")
+                        continue
+
+                    mensaje = {"tipo": "msj",
+                               "privado": False,
+                               "user": self.usuario,
+                               "sala": self.sala,
+                               "content": sticker}
+                    
+                    self.sock.sendto(json.dumps(mensaje).encode(), (SERVER_IP, SERVER_PORT))
+                elif (texto_entrada.startswith("@")): # Si es un mensaje privado
+                    partes = texto_entrada.split(" ", 1)
+                    
+                    if (len(partes) < 2): # Si el formato no es el correcto
+                        print("[Sistema] Formato: @usuario mensaje")
+                        continue
+
+                    if (partes[1].startswith("/sticker")): # Si es un sticker privado
+                        partes2 = partes[1].split(" ")
+
+                        if (len(partes2) < 2): # Si el formato no es correcto
+                            print("[Sistema] Formato: /sticker nombre_sticker")
+                            continue
+
+                        _, nombre_sticker = partes2
+
+                        sticker = self.sticker(nombre_sticker)
+
+                        if (not sticker): # Si el sticker no existe
+                            print("[Sistema] El sticker no existe")
+                            continue
+
+                        destinatario, _ = partes
+                        destinatario = destinatario[1:] # Quitar '@'
+                        mensaje = {"tipo": "msj",
+                                   "privado": True,
+                                   "from": self.usuario,
+                                   "to": destinatario,
+                                   "content": sticker,
+                                   "sala": self.sala}
+                        
+                        self.sock.sendto(json.dumps(mensaje).encode(), (SERVER_IP, SERVER_PORT))
+                    
+                        print(f"{ORANGE}[Tú -> {destinatario}]{RESET}: {sticker}")
+                        continue
+                    else:
+                        destinatario, contenido = partes
+                        destinatario = destinatario[1:] # Quitar '@'
+                        mensaje = {"tipo": "msj",
+                                "privado": True,
+                                "from": self.usuario,
+                                "to": destinatario,
+                                "content": contenido,
+                                "sala": self.sala}
+                        
+                        self.sock.sendto(json.dumps(mensaje).encode(), (SERVER_IP, SERVER_PORT))
+                        
+                        print(f"{ORANGE}[Tú -> {destinatario}]{RESET}: {contenido}")
+                        continue
+                else: # Si es un mensaje público
+                    mensaje = {"tipo": "msj",
+                               "privado": False,
+                               "user": self.usuario,
+                               "sala": self.sala,
+                               "content": texto_entrada}
+                    
+                    self.sock.sendto(json.dumps(mensaje).encode(), (SERVER_IP, SERVER_PORT))
+            except KeyboardInterrupt: # El usuario presiona Ctrl + C (salir de la sala)
+                salir = {"tipo": "salir",
+                         "user": self.usuario,
+                         "sala": self.sala}
+                
+                self.sock.sendto(json.dumps(salir).encode(), (SERVER_IP, SERVER_PORT))
+                self.activo = False
+                
+                sys.exit(0)
+
+    def iniciar(self) -> None:
+        """Método para inicializar los hilos. Un hilo para recibir mensajes en segundo plano y uno principal para enviar."""
+        threading.Thread(target=self.recibir, daemon=True).start()
+        self.enviar()
+
+def main() -> None:
+    usuario = input("Antes de unirte a la sala, escribe tu nombre de usuario:\nUsuario: ")
+    cliente = Cliente(usuario, "general")
+    cliente.iniciar()
+
+if (__name__ == "__main__"):
+    main()
---
+++ Programas en Python/Discord en python/Archivos buenos/Una sala (vu) v7 - estable/servidor.py
@@ -0,0 +1,119 @@
+import socket, json, threading
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+BLUE = "\033[94m"
+RESET = "\033[0m"
+
+# Dirección y puerto del servidor
+HOST, PORT = "0.0.0.0", 5007 # El servidor escucha en todas las interfaces y en el puerto 5007
+usuarios = {"general": {}} # Lista de usuarios por sala: {"general": {"usuario": (ip, puerto)}}
+
+# Crear socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Se crea un socket UDP
+sock.bind((HOST, PORT)) # Se vincula el socket al puerto del servidor
+
+print("Servidor de chat unicast activo...")
+
+def enviar_unicast(data: dict, addr: tuple) -> None:
+    """
+    Envía un mensaje a un cliente específico
+    
+    Parameters
+    ----------
+    data : dict
+        Mensaje a enviar al cliente.
+    addr : tuple
+        Dirección IP y puerto del usuario.
+    """
+    sock.sendto(json.dumps(data).encode(), addr)
+
+def enviar_publico(data: dict, sala: str) -> None:
+    """
+    Envía un mensaje a todos los usuarios de la sala
+    
+    Parameters
+    ----------
+    data : dict
+        Mensaje a enviar a todos los usuarios de la sala.
+    sala : str
+        Sala a la que se envía el mensaje.
+    """
+    for user_addr in usuarios[sala].values():
+        enviar_unicast(data, user_addr)
+
+def manejar_cliente() -> None:
+    """
+    Loop principal del servidor para recibir mensajes
+    """
+    while (True):
+        data, addr = sock.recvfrom(4096) # Recibe datagramas de cualquier cliente.
+        try:
+            msj = json.loads(data.decode()) # Se decodifica el mensaje en JSON
+        except:
+            # Se ignora si la decodificación falla
+            continue
+
+        tipo = msj.get("tipo") # Se estrae el tipo del mensaje
+        user = msj.get("user") # Se extrae el usuario que envía el mensaje
+        sala = msj.get("sala", "general") # Se extrae la sala a la que se envía el mensaje
+
+        # Crear sala si no existe
+        if (sala not in usuarios):
+            usuarios[sala] = {}
+
+        # Usuario entra a la sala
+        if (tipo == "inicio"):
+            if (user not in usuarios[sala]): # Si el usuario no esta en la sala anteriormente
+                usuarios[sala][user] = addr # Se agrega el usuario a la sala (se guarda la IP y puerto del usuario)
+
+                aviso = {"tipo": "aviso", "sala": sala, "content": f"{GREEN}[+]{RESET}{BLUE}[{user}]{RESET} se ha unido a la sala"}
+                
+                enviar_publico(aviso, sala) # Se envía el aviso de entrada de un usuario
+
+            # Si el usuario ya se encuentra en la sala solo se envía la lista de usuarios actual
+            lista = {"tipo": "usuarios", "sala": sala, "lista": list(usuarios[sala].keys())}
+            
+            enviar_publico(lista, sala) # Se envía la lista actualizada
+        # Mensaje público
+        elif (tipo == "msj" and not msj.get("privado", False)):
+            enviar_publico(msj, sala)
+        # Mensaje privado
+        elif (tipo == "msj" and msj.get("privado", False)):
+            destino = msj.get("to")
+            
+            if (destino in usuarios[sala]): # Si es destinatario esta en la sala
+                enviar_unicast(msj, usuarios[sala][destino]) # Se envía el mensaje al destinatario
+                #enviar_unicast(msj, addr) # Se envía una copia del mensaje al remitente
+            else: # Si el destinatario no esta en la sala
+                error = {"tipo": "aviso", "sala": sala, "content": f"[Sistema] Usuario '{destino}' no está conectado."}
+                
+                enviar_unicast(error, addr) # Se envía un mensaje de error
+        # Usuario sale
+        elif (tipo == "salir"):
+            if (user in usuarios[sala]): # Si el usuario se encuentra en la sala
+                usuarios[sala].pop(user) # Se elimina el usuario de la sala
+                
+                aviso = {"tipo": "aviso", "sala": sala, "content": f"{RED}[-]{RESET}{BLUE}[{user}]{RESET} ha abandonado la sala"}
+                
+                enviar_publico(aviso, sala) # Se envía el aviso de salida de un usuario
+
+            lista = {"tipo": "usuarios", "sala": sala, "lista": list(usuarios[sala].keys())}
+            
+            enviar_publico(lista, sala) # Se envía la lista actualizada
+        # Usuario graba audio
+        elif (tipo == "audio"):
+            aviso = {"tipo": "aviso", "sala": sala, "content": f"{GREEN}[🎙️][{user}]{RESET} ha enviado un audio"}
+
+            enviar_publico(aviso, sala)
+
+def main() -> None:
+    # Hilo principal del servidor
+    threading.Thread(target=manejar_cliente, daemon=True).start()
+
+    # Mantener el servidor activo
+    while True:
+        pass
+
+if (__name__ == "__main__"):
+    main()
